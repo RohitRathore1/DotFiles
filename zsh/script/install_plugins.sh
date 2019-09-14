206acd55978355e87e722496eaa4d4de4adbb4a0
TeAmP0is0N <rohitrathore.imh55@gmail.com>
2019-09-14 09:23:45 +0530

    Update

---
+++ zsh/script/install_plugins.sh
@@ -0,0 +1,5 @@
+# Install oh my zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+mkdir -p ~/.zfunctions
